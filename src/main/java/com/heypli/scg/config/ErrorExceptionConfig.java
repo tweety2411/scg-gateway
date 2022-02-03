c9e6a9af3717e5a1bba036ff8bc3d1b86e2a0d35
tweety1121 <baejisuk524@gmail.com>
2022-02-03 18:41:33 +0900

    global exception 추가

---
+++ src/main/java/com/heypli/scg/config/ErrorExceptionConfig.java
@@ -0,0 +1,18 @@
+package com.heypli.scg.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heypli.scg.GlobalExceptionHandler.GlobalExceptionHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ErrorExceptionConfig {
+    private final ObjectMapper objectMapper;
+    @Bean
+    public ErrorWebExceptionHandler globalExceptionHandler() {
+        return new GlobalExceptionHandler(objectMapper);
+    }
+}
